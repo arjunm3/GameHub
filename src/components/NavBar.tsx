e43659c5b51e1fdc0350be7a3d5a0b116cbc7123
Arjun M <arjunm3393@gmail.com>
2024-05-20 15:42:22 +0530

    Adding name of the application

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { HStack, Heading, Image } from '@chakra-ui/react'
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
@@ -10,6 +10,7 @@ const NavBar = ({ onSearch }: Props) => {
     return (
         <HStack padding='10px'>
             <Image src={logo} boxSize='60px' />
+            <Heading as={'h1'} fontSize='2xl' whiteSpace='nowrap'>Arjun's GameHub</Heading>
             <SearchInput onSearch={onSearch} />
             <ColorModeSwitch />
         </HStack>
